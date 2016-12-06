6bc6361c47a7a37f965b9b3c4f4701becf7f2e8e
ryanzhao1993 <ryanzhao1993@qq.com>
2016-12-06 14:14:37 +0800

    update setTimeout

--- 03.es/setTimeout.js
+++ 03.es/setTimeout.js
@@ -1,5 +1,7 @@
 //setTimeout异步方法，可以通过多个参数进行传参
 function fn(a, b) {
-    console.log('setTimout', a, b);
+    console.log(a, b);
 }
-setTimeout(fn, 2000, 'aaa', 'bbb');  //setTimout aaa bbb
+setTimeout(fn, 2000, 'aaa', 'bbb');  // aaa bbb
+
+setInterval(fn, 1000, 'ccc', 'ddd');  // ccc ddd
