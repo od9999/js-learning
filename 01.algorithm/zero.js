1a9cfe9a6f2d87a972dca59bd447626975c93536
ryanzhao1993 <ryanzhao1993@qq.com>
2016-12-06 17:40:50 +0800

    add zero currying

---
+++ 01.algorithm/zero.js
@@ -0,0 +1,15 @@
+// 区分 +0 和 -0
+console.log( +0 === -0 ); // true
+
+console.log(1 / -0);   // -Infinity
+console.log(1 / +0);   // Infinity
+
+function isNegativeZero(num){
+    return num === 0 && (1 / num < 0);
+}
+function isPositiveZero(num){
+    return num === 0 && (1 / num > 0);
+
+}
+console.log(isNegativeZero(-0));
+console.log(isPositiveZero(+0));
