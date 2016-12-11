33d9ca867f3c6624980ed6db21e5680ca5843b67
ryanzhao1993 <ryanzhao1993@qq.com>
2016-12-11 16:59:13 +0800

    add class

--- 03.es/array.js
+++ 03.es/array.js
@@ -1,10 +1,7 @@
 // 找出数组最大、最小的一项
-
 let arr = [5, -9, 18, 2, 0, 9];
 console.log(Math.max(...arr));
 console.log(Math.max.apply(null, arr));
-
-
 
 // 复制数组
 let arr2 = arr.concat();
---
+++ 03.es/new.js
@@ -0,0 +1,24 @@
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+    // this = new Boolean(false);  // ReferenceError: Invalid left-hand side in assignment
+    return new Boolean(false);
+    this.gender = 'male';
+}
+
+let p = new Person('od', 23);
+console.log(p);   // [Boolean: false]
+
+
+
+
+function Person2(name, age){
+    this.name = name;
+    this.age = age;
+    return Object.create(null);  // Person2.prototype不在对象的原型链上
+
+}
+
+let p2 = new Person2('od', 23);
+console.log(p2);                // {}
+console.log(p2 instanceof Person2);   // false
