95ccfba568bbd2ad0fe0e32fbda0e32abb7f9d9e
zhaorong <zhaorong@haodf.com>
2017-03-30 09:42:15 +0800

    commit

--- 02.sort/selectSort.js
+++ 02.sort/selectSort.js
@@ -1,17 +1,15 @@
 function insertSort (arr) {
 	let minIndex;
-
 	for (let i = 0; i < arr.length - 1; i++) {
         minIndex = i;
 		for (let j = i + 1; j < arr.length; j++) {
-			if (arr[i] > arr[j]) {
+			if (arr[minIndex] > arr[j]) {
 				minIndex = j;
 			}
 		}
 		if (i !== minIndex) {
             [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
 		}
-
 	}
 	return arr;
 }
