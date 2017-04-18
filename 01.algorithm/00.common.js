0b8560a30e5a12286cad8e4d4cf134f1d65119c6
ryanzhao1993 <ryanzhao1993@qq.com>
2017-04-18 10:19:26 +0800

    commit

--- 01.algorithm/00.common.js
+++ 01.algorithm/00.common.js
@@ -59,4 +59,10 @@ function getMaxDuplicateChar(str) {
 console.log(getMaxDuplicateChar('afzzzzzzzzzzjghdfraazzzzzzzzzzzzaasdenas'));
 
 
+// 二叉树 给出中序、前序 找出后续
+
+// 链表从后到前 输出。栈 或 递归
+
+// 旋转数组的最小数字  [3, 4, 5 ,1 ,2]的最小数字是1
+// 两个指针index1 = 0 index2 = length - 1 二分查找 不断改变index1和index2最后 index2就是找到的值
 
---
+++ 01.algorithm/09.fibonacci.js
@@ -0,0 +1,16 @@
+/**
+ * 斐波那契数列
+ * f(n) = 0 (n = 0)
+ * f(n) = 1 (n = 1)
+ * f(n) = f(n - 1) + f(n - 2) (n > 1)
+ * 类似问题： 一只青蛙 一次能跳2个阶梯 或者跳1个阶梯 如果有n阶梯 有几种跳法？
+ */
+let fibonacci = (function() {
+    let memory = [0, 1];
+    return function(n) {
+        if(memory[n] !== undefined) {
+            return memory[n];
+        }
+        return memory[n] = (n === 0 || n === 1) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+    };
+})();
