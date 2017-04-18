d4f13005a8e9c3455d8718222b8d3df9e8d159ab
zhaorong <zhaorong@haodf.com>
2017-04-18 14:46:08 +0800

    commit

--- 03.es/10.Object/00.asign.js
+++ 03.es/10.Object/00.asign.js
@@ -10,12 +10,14 @@ let target = {
  * 只会拷贝源对象自身的并且可枚举的属性到目标对象身上
  */
 
+
 let source1 = {
 	name: 'od',
 	sayHello: function() {
 		console.log(this.name);
 	}
 };
+
 
 let source2 = {
 	age: 23
@@ -30,10 +32,40 @@ Object.defineProperty(source2, 'gender', {
 		console.log('source2 gender set------------');
 	}
 });
+let symbol = Symbol('source2');
+Object.defineProperty(source2, symbol, {
+	enumerable: true,
+	value: 'source2-symbol'
+});
 
-Object.assign(target, source1, source2);
+
+let source3 = {
+	hobbies: {
+		a: 'aaa',
+		b: 'bbb',
+		c: {
+			e: 'eee',
+			f: 'fff'
+		}
+	}
+};
+
+Object.assign(target, source1, source2, source3);
 
 console.log(target);
-console.log(target.sayHello === source1.sayHello);	// true
-console.log(target.gender === source2.gender);	// true
+console.log(target.sayHello === source1.sayHello);		// true
+console.log(target.gender === source2.gender);			// true
+console.log(target[symbol] === source2[symbol]);		// true
+console.log(target.hobbies.c === source3.hobbies.c);	// true
 
+source3.hobbies.c = {
+	g: 'ggg source3'
+};
+
+console.log(target.hobbies.c);
+
+target.hobbies.c = {
+	g: 'ggg target'
+};
+
+console.log(source3.hobbies.c);
