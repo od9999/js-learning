a663984ece0d94efc11e8d8758afa9ac952827ea
ryanzhao1993 <ryanzhao1993@qq.com>
2017-10-10 20:04:50 +0800

    commit

--- 03.es/09.this/00.node.js
+++ 03.es/09.this/00.node.js
@@ -1,3 +1,8 @@
+/*
+ 根据es3和非严格的es5对函数调用(不是方法调用)规定，调用上下文是全局对象
+ 严格模式下，调用上下文是undefined
+ */
+
 console.log(this);	// node中 如果是文件运行 得到{}，如果是repl环境 是global
 
 (function() {
---
+++ 03.es/18.bind/00.js
@@ -0,0 +1,25 @@
+function Person(name) {
+    this.name = name;
+    console.log(this === global);
+}
+
+console.log(new Person('Person'));
+console.log(Person('Person'));
+console.log(global.name);
+console.log('-------');
+
+let context = {};
+let PersonBind = Person.bind(context);
+let personBind = PersonBind('PersonBind');
+let newPersonBind = new PersonBind('newPersonBind');
+console.log(personBind);
+console.log(newPersonBind);
+console.log('------');
+
+let context2 = {};
+let PersonBind2 = PersonBind.bind(context2);
+let personBind2 = PersonBind2('PersonBind2');
+let newPersonBind2 = new PersonBind2('newPersonBind2');
+console.log(personBind2);
+console.log(newPersonBind2);
+console.log('------');
